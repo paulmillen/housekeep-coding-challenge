1e1b97d209e10ab4ee204d09acf26c604ce6d5d0
paulmillen <pwmillen@gmail.com>
2017-10-03 17:14:50 +0100

    hello world tested with protractor

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -3,4 +3,9 @@ describe("app", () => {
     browser.get('/');
     expect(browser.getTitle()).toEqual("Booking App");
   });
+
+  it("should say 'Hello World' on the page", () => {
+    browser.get('/');
+    expect($$("p").first().getText()).toEqual("Hello World")
+  });
 });
