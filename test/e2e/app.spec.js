bb5f73e99b333d6432157f6a3a9941418fb24b11
paulmillen <pwmillen@gmail.com>
2017-10-03 17:37:51 +0100

    bookingController created, wired to index

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,11 +1,14 @@
 describe("app", () => {
-  it("should get home page title", () => {
-    browser.get('/');
-    expect(browser.getTitle()).toEqual("Booking App");
-  });
-
   it("should say 'Hello World' on the page", () => {
     browser.get('/');
     expect($$("p").first().getText()).toEqual("Hello World")
   });
 });
+
+describe('Booking', () => {
+  it('has a day', () => {
+    browser.get('/');
+    const day = $('#day');
+    expect(day.getText()).toEqual('Day')
+  })
+})
