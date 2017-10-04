04ea099c50aa55208a88b74edbc3a0dba0abf034
paulmillen <pwmillen@gmail.com>
2017-10-04 15:13:53 +0100

    e2e tests for days and times added

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -5,4 +5,10 @@ describe("app", () => {
     expect(days.first().getText()).toEqual('2016-12-05')
     expect(days.last().getText()).toEqual('2016-12-11')
   });
+  it('has buttons with available times', () => {
+    browser.get('/');
+    const times = $$('#days button');
+    expect(times.first().getText()).toEqual('08:00:00 - 08:30:00')
+    expect(times.last().getText()).toEqual('14:00:00 - 15:00:00')
+  });
 });
