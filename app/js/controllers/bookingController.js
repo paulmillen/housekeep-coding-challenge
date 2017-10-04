118b61d6eb7bdd3cecae9c7599e8ac6cb81651bb
paulmillen <pwmillen@gmail.com>
2017-10-04 12:09:06 +0100

    created $scope.booking and booking post request

--- app/js/controllers/bookingController.js
+++ app/js/controllers/bookingController.js
@@ -4,4 +4,21 @@ bookingApp.controller('BookingController', ['$scope','BookingService', function(
     $scope.week = response.data;
    });
 
+   $scope.booking = {
+     day: "",
+     startTime: {
+       start: "",
+       end: ""
+     },
+     visitDuration: 1,
+     propertyId: "ealdk1f9"
+   };
+
+   $scope.submit = function() {
+     $http.post("https://private-anon-2e5e7d446d-housekeepavailability.apiary-mock.com/book/", $scope.booking)
+     .then(function(response){
+        $scope.confirmation = response.data;
+      })
+    };
+
 }]);
