fc295010d008b3ea91d98cbd8d284bc74c7822f4
paulmillen <pwmillen@gmail.com>
2017-10-04 11:41:10 +0100

    displaying days in index, filtered by availability

--- app/js/controllers/bookingController.js
+++ app/js/controllers/bookingController.js
@@ -1,3 +1,7 @@
 bookingApp.controller('BookingController', ['$scope','BookingService', function($scope, BookingService) {
-  BookingService.getAll(function(response) { $scope.week = response.data });
+
+  BookingService.getAll(function(response) {
+    $scope.week = response.data;
+   });
+
 }]);
